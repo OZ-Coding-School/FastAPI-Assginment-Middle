3b36dde34abcb5cceb9ab5d12c82e439f7b7a3bf
meoyoug <eodyd426@gmail.com>
2025-03-17 15:02:54 +0900

    - add utils   - add create_access_token()

---
+++ src/utils/jwt.py
@@ -0,0 +1,17 @@
+from datetime import timedelta, datetime, timezone
+
+import jwt
+
+SECRET_KEY = "radadssadadqwe1231eszdas12e1dasdsad12"
+ALGORITHM = "HS256"
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
