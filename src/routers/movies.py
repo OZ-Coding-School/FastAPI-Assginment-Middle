c67f289705a6269ebf70efab5abf89cd4093fa1b
meoyoug <eodyd426@gmail.com>
2025-03-20 22:30:51 +0900

    - app/routers/user_router.py   - call hash_password() at create_user(), update_user()   - Convert Non-database Model to Database Model(UserModel -> User)   - Change router function code according to model change
    - app/routers/movie_router.py
      - Convert Non-database Model to Database Model(MovieModel -> Movie)
      - Change router function code according to model change

--- src/routers/movies.py
+++ src/routers/movies.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 from fastapi import Query, Path, HTTPException, APIRouter
 
-from src.models.movies import MovieModel
+from src.models.movies import Movie
 from src.schemas.movies import MovieResponse, CreateMovieRequest, MovieSearchParams, MovieUpdateRequest
 
 
@@ -11,23 +11,19 @@ movie_router = APIRouter(prefix="/movies", tags=["movies"])
 
 @movie_router.post('', response_model=MovieResponse, status_code=201)
 async def create_movie(data: CreateMovieRequest):
-	movie = MovieModel.create(**data.model_dump())
+	movie = await Movie.create(**data.model_dump())
 	return movie
 
 
 @movie_router.get('', response_model=list[MovieResponse], status_code=200)
 async def get_movies(query_params: Annotated[MovieSearchParams, Query()]):
 	valid_query = {key: value for key, value in query_params.model_dump().items() if value is not None}
-	
-	if valid_query:
-		return MovieModel.filter(**valid_query)
-	
-	return MovieModel.all()
+	return await Movie.filter(**valid_query).all()
 
 
 @movie_router.get('/{movie_id}', response_model=MovieResponse, status_code=200)
 async def get_movie(movie_id: int = Path(gt=0)):
-	movie = MovieModel.get(id=movie_id)
+	movie = await Movie.get_or_none(id=movie_id)
 	if movie is None:
 		raise HTTPException(status_code=404)
 	return movie
@@ -35,17 +31,19 @@ async def get_movie(movie_id: int = Path(gt=0)):
 
 @movie_router.patch('/{movie_id}', response_model=MovieResponse, status_code=200)
 async def edit_movie(data: MovieUpdateRequest, movie_id: int = Path(gt=0)):
-	movie = MovieModel.get(id=movie_id)
+	movie = await Movie.get_or_none(id=movie_id)
 	if movie is None:
 		raise HTTPException(status_code=404)
-	movie.update(**data.model_dump())
+	valid_params = {key: value for key, value in data.model_dump().items() if value is not None}
+	await movie.update_from_dict(valid_params)
+	await movie.save()
 	return movie
 
 
 @movie_router.delete('/{movie_id}', status_code=204)
 async def delete_movie(movie_id: int = Path(gt=0)):
-	movie = MovieModel.get(id=movie_id)
+	movie = await Movie.get(id=movie_id)
 	if movie is None:
 		raise HTTPException(status_code=404)
-	movie.delete()
+	await movie.delete()
 	return
--- src/routers/users.py
+++ src/routers/users.py
@@ -4,22 +4,25 @@ from typing import Annotated
 from fastapi import HTTPException, Query, APIRouter, status, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
-from src.models.users import UserModel
-from src.schemas.users import UserUpdateRequest, UserCreateRequest, UserSearchParams, Token
-from src.utils.jwt import create_access_token, get_current_user
+from src.models.users import User
+from src.schemas.users import UserUpdateRequest, UserCreateRequest, UserSearchParams, Token, UserResponse
+from src.utils.auth import authenticate, get_current_user, hash_password
+from src.utils.jwt import create_access_token
 
 user_router = APIRouter(prefix='/users', tags=["users"])
 
 
 @user_router.post('')
 async def create_user(data: UserCreateRequest):
-	user = UserModel.create(**data.model_dump())
+	data = data.model_dump()
+	data['hashed_password'] = hash_password(data.pop('password'))
+	user = await User.create(**data)
 	return user.id
 
 
 @user_router.get('')
-async def get_all_users():
-	result = UserModel.all()
+async def get_all_users() -> list[UserResponse]:
+	result = await User.filter().all()
 	if not result:
 		raise HTTPException(status_code=404)
 	return result
@@ -27,7 +30,7 @@ async def get_all_users():
 
 @user_router.post('/login', response_model=Token)
 async def login_user(data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-	user = UserModel.authenticate(data.username, data.password)
+	user = await authenticate(data.username, data.password)
 	if not user:
 		raise HTTPException(
 			status_code=status.HTTP_401_UNAUTHORIZED,
@@ -35,36 +38,39 @@ async def login_user(data: Annotated[OAuth2PasswordRequestForm, Depends()]):
 			headers={"WWW-Authenticate": "Bearer"},
 		)
 	access_token = create_access_token(data={"user_id": user.id})
-	user.update(last_login=datetime.now())
+	user.last_login = datetime.now()
+	await user.save()
 	return Token(access_token=access_token, token_type="bearer")
 
 
 @user_router.get('/search')
-async def search_users(query_params: Annotated[UserSearchParams, Query()]):
+async def search_users(query_params: Annotated[UserSearchParams, Query()]) -> list[UserResponse]:
 	valid_query = {key: value for key, value in query_params.model_dump().items() if value is not None}
-	filtered_users = UserModel.filter(**valid_query)
+	filtered_users = await User.filter(**valid_query).all()
 	if not filtered_users:
 		raise HTTPException(status_code=404)
 	return filtered_users
 
 
 @user_router.get('/me')
-async def get_user(user: Annotated[UserModel, Depends(get_current_user)]):
-	return user
+async def get_user(user: Annotated[User, Depends(get_current_user)]) -> UserResponse:
+	return UserResponse(id=user.id, username=user.username, age=user.age, gender=user.gender)
 
 
 @user_router.patch('/me')
 async def update_user(
-	user: Annotated[UserModel, Depends(get_current_user)],
+	user: Annotated[User, Depends(get_current_user)],
 	data: UserUpdateRequest,
-):
-	if user is None:
-		raise HTTPException(status_code=404)
-	user.update(**data.model_dump())
-	return user
+) -> UserResponse:
+	update_data = {key: value for key, value in data.model_dump().items() if value is not None}
+	if 'password' in update_data.keys():
+		update_data['hashed_password'] = hash_password(update_data.pop('password'))
+	user = await user.update_from_dict(data=update_data)
+	await user.save()
+	return UserResponse(id=user.id, username=user.username, age=user.age, gender=user.gender)
 
 
 @user_router.delete('/me')
-async def delete_user(user: Annotated[UserModel, Depends(get_current_user)],):
-	user.delete()
+async def delete_user(user: Annotated[User, Depends(get_current_user)],):
+	await user.delete()
 	return {'detail': 'Successfully Deleted.'}
