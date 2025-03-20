f702188dacb7e1b45f5cd9ca7abd453174a56403
meoyoug <eodyd426@gmail.com>
2025-03-20 22:20:20 +0900

    - app/configs/base.py   - create Config BaseSettings Class
    - app/configs/__init__.py
      - add get_config()
    
    - app/configs/database.py
      - add TORTOISE ORM settings
      - add initialize_tortoise()
    
    - main.py
      - call initialize_tortoise()
      - delete UserModel.create_dummy()
      - delete MovieModel.create_dummy()
    
    - .env
      - add example Environment variable

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from fastapi import FastAPI
 
-from src.models.movies import MovieModel
-from src.models.users import UserModel
+from src.configs.database import initialize_tortoise
 from src.routers.movies import movie_router
 from src.routers.users import user_router
 
 app = FastAPI()
 
-UserModel.create_dummy()
-MovieModel.create_dummy()
 
 app.include_router(user_router)
 app.include_router(movie_router)
 
+initialize_tortoise(app=app)
 
 if __name__ == '__main__':
 	import uvicorn
