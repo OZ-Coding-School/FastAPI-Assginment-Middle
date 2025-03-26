ec5cd957dba95ebc9a43b1bbd5ecc12bd6db380e
meoyoug <eodyd426@gmail.com>
2025-03-26 11:51:16 +0900

    - src/schemas/users.py   - add profile_image_url field to UserResponse Pydantic Model
    - src/routers/user_router.py
      - add router function("/users/me/profile_image")
        - register_profile_image()

--- src/routers/users.py
+++ src/routers/users.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 from typing import Annotated
 
-from fastapi import HTTPException, Query, APIRouter, status, Depends
+from fastapi import HTTPException, Query, APIRouter, status, Depends, UploadFile
 from fastapi.security import OAuth2PasswordRequestForm
 
 from src.models.users import User
 from src.schemas.users import UserUpdateRequest, UserCreateRequest, UserSearchParams, Token, UserResponse
 from src.utils.auth import authenticate, get_current_user, hash_password
+from src.utils.file import validate_image_extension, delete_file, upload_file
 from src.utils.jwt import create_access_token
 
 user_router = APIRouter(prefix='/users', tags=["users"])
@@ -71,6 +72,29 @@ async def update_user(
 
 
 @user_router.delete('/me')
-async def delete_user(user: Annotated[User, Depends(get_current_user)],):
+async def delete_user(user: Annotated[User, Depends(get_current_user)]):
 	await user.delete()
 	return {'detail': 'Successfully Deleted.'}
+
+
+@user_router.post("/me/profile_image", status_code=200)
+async def register_profile_image(image: UploadFile, user: Annotated[User, Depends(get_current_user)]) -> UserResponse:
+	validate_image_extension(image)
+	prev_image_url = user.profile_image_url
+	try:
+		image_url = await upload_file(image, "users/profile_images")
+		user.profile_image_url = image_url
+		await user.save()
+		
+		if prev_image_url is not None:
+			delete_file(prev_image_url)
+
+		return UserResponse(
+			id=user.id,
+			username=user.username,
+			age=user.age,
+			gender=user.gender,
+			profile_image_url=user.profile_image_url
+		)
+	except Exception as e:
+		raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
