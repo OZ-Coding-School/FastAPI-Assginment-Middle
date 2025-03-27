3c109e04f76f11f32f17cb1d30b66fc42ca7193c
meoyoug <eodyd426@gmail.com>
2025-03-27 15:50:18 +0900

    - src/schemas/reviews.py   - add ReviewResponse
    - src/schemas/likes.py
      - ReviewLikeResponse
      - ReviewLikeCountResponse

---
+++ src/schemas/likes.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+
+class ReviewLikeResponse(BaseModel):
+	id: int
+	user_id: int
+	review_id: int
+	is_liked: bool
+
+
+class ReviewLikeCountResponse(BaseModel):
+	review_id: int
+	like_count: int
