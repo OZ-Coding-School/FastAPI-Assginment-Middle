4ec708005003560ca323afc93dc1bd9becd1c7c5
meoyoug <eodyd426@gmail.com>
2025-04-07 17:23:04 +0900

    - import signals - include router(notification_router)

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from fastapi import FastAPI
 from src.configs.database import initialize_tortoise
 from src.routers.movies import movie_router
 from src.routers.likes import like_router
+from src.routers.notifications import notification_router
 from src.routers.reviews import review_router
 from src.routers.users import user_router
+import src.signals
 
 app = FastAPI()
 
@@ -13,6 +15,7 @@ app.include_router(user_router)
 app.include_router(movie_router)
 app.include_router(review_router)
 app.include_router(like_router)
+app.include_router(notification_router)
 
 initialize_tortoise(app=app)
 
